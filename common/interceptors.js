fbe442f7959c114d69e48950a5e1abe9eea7ff27
Syed Arbaz Ahmed <syedarbaz.ahmed@sybrid.com>
2021-12-30 16:44:57 +0500

    Minor changes

--- common/interceptors.js
+++ common/interceptors.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const interceptors = express();
 
+
+// Include all interceptors here...
+
 interceptors.use((req, res, next) => {
+    console.log('this is me');
   next();
 })
 
